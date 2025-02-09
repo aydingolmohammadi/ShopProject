614f108adb0270ff825efc87b8f50d35b9a0b952
a.golmohammadi.rahbord <a.golmohammadi.rahbord@gmail.com>
2025-02-10 00:42:25 +0330

    update gitignore

--- Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf61c21c1faa0f80fa4a0091d4c614986a340555")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+843b2a5ec77ece1dad3449180787c076b8a52160")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
